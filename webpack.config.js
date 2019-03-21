eecda285b3032a36243d04fc323a0d711d347b79
Philipp Beisel <infobeisel@gmail.com>
2019-03-21 11:43:49 +0000

    created intial project setup with example code. uses nodejs, babel and already imports pixijs.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+	 path: path.resolve('./build/'),
+	 filename: 'bundle.js'
+	},
+	
+	module: {
+    rules: [
+	{ 
+      test: /\.js$/, 
+      exclude: /node_modules/, 
+      loader: "babel-loader"
+    }
+	]
+  }
+};
